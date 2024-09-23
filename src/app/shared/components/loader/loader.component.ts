f84ef19ac8cd6614abb4a4220d572627483b2f75
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-09-23 21:22:25 +0100

    feat:  loader component

---
+++ src/app/shared/components/loader/loader.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'loader',
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.css',
+  standalone: true,
+})
+export class LoaderComponent {}
