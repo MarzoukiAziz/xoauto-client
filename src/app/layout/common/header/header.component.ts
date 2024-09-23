777fbedef9cec26450e6c2bec180f85ab278fb32
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-09-23 02:23:31 +0100

    feat: add fav icons

--- src/app/layout/common/header/header.component.ts
+++ src/app/layout/common/header/header.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
