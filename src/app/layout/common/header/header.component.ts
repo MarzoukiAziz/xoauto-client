956b04c39cd908e08c198f97c8af54f43afbea53
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-09-23 03:13:14 +0100

    feat: improve navigation with header

--- src/app/layout/common/header/header.component.ts
+++ src/app/layout/common/header/header.component.ts
@@ -5,4 +5,33 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isMobileNavExpanded = false;
+
+  toggleMobileNav() {
+    this.isMobileNavExpanded = !this.isMobileNavExpanded;
+    document.body.classList.toggle('locked', this.isMobileNavExpanded);
+  }
+
+  menuItems: any[] = [
+    { label: 'Acceuil', link: '/' },
+    {
+      label: 'Neuf',
+      link: '#',
+      children: [
+        { label: 'Recherche', link: '/n/search' },
+        { label: 'Marques', link: '/marques' },
+        { label: 'Comparateur', link: '/n/comparateur' },
+      ],
+    },
+    {
+      label: 'Occasions',
+      children: [
+        { label: 'Recherche', link: 'ads' },
+        { label: 'Annonces Du Jour', link: 'today-ads' },
+        { label: 'Comparateur', link: 'ads-comparator' },
+      ],
+    },
+    { label: 'Magazine', link: '/magazine' },
+  ];
+}
