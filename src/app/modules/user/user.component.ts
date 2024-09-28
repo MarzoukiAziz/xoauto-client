6e076187743b2abe2460fbc2cbe228c920f1e460
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-09-28 21:10:29 +0100

    feat: track route change on user panel

--- src/app/modules/user/user.component.ts
+++ src/app/modules/user/user.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { UserService } from './user.service';
-import { Router, RouterModule } from '@angular/router';
+import { Router, RouterModule, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { User } from './user.types';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, takeUntil, filter } from 'rxjs';
 import { NewAdBannerComponent } from '../../shared/components/new-ad-banner/new-ad-banner.component';
 
 @Component({
@@ -18,7 +18,7 @@ import { NewAdBannerComponent } from '../../shared/components/new-ad-banner/new-
     NewAdBannerComponent,
   ],
 })
-export class UserComponent {
+export class UserComponent implements OnInit, OnDestroy {
   user!: User;
   selectedSection = 'dashboard';
   private _unsubscribeAll: Subject<any> = new Subject<any>();
@@ -34,13 +34,27 @@ export class UserComponent {
   }
 
   ngOnInit(): void {
+    // Set selected section based on initial URL
     const segments = this.router.url.split('/');
     this.selectedSection = segments[2] || '';
+
+    // Subscribe to user data
     this._userService.user$
       .pipe(takeUntil(this._unsubscribeAll))
       .subscribe((user: User) => {
         this.user = user;
+        this._changeDetectorRef.markForCheck();
+      });
 
+    // Subscribe to router events to track route changes
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        takeUntil(this._unsubscribeAll)
+      )
+      .subscribe((event: NavigationEnd) => {
+        const newSegments = event.urlAfterRedirects.split('/');
+        this.selectedSection = newSegments[2] || '';
         this._changeDetectorRef.markForCheck();
       });
   }
