a51b0f8b10cd25eb7682a030ce9efe2af685bbaa
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-09-29 18:37:09 +0100

    feat:  no ads component

---
+++ src/app/modules/ad/components/no-ads/no-ads.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'no-ads',
+  templateUrl: './no-ads.component.html',
+  styleUrl: './no-ads.component.css',
+  standalone: true,
+})
+export class NoAdsComponent {}
