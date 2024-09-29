63213f41c80c5b358a6fb5dd931c0034d572b04e
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-09-29 20:41:05 +0100

    feat: adapt no ads for custom message

--- src/app/modules/ad/components/no-ads/no-ads.component.ts
+++ src/app/modules/ad/components/no-ads/no-ads.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'no-ads',
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './no-ads.component.css',
   standalone: true,
 })
-export class NoAdsComponent {}
+export class NoAdsComponent {
+  @Input() message: string =
+    "Aucune annonce de voitures d'occasion n'a été publiée aujourd'hui.";
+}
