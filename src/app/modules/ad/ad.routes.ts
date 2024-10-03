68100555290b10590316415d75537fdab090154c
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-10-04 00:13:11 +0100

    feat: add ad detail to routes

--- src/app/modules/ad/ad.routes.ts
+++ src/app/modules/ad/ad.routes.ts
@@ -1,8 +1,23 @@
 import { inject } from '@angular/core';
-import { Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, Routes } from '@angular/router';
 import { AdService } from './ad.service';
 import { TodayAdsComponent } from './today-ads/today-ads.component';
 import { ComparatorComponent } from './comparator/comparator.component';
+import { DetailComponent } from './detail/detail.component';
+import { catchError, throwError } from 'rxjs';
+
+const adResolver = (route: ActivatedRouteSnapshot) => {
+  const adService = inject(AdService);
+  const router = inject(Router);
+
+  return adService.getAdById(route.paramMap.get('adid') ?? '').pipe(
+    catchError((error) => {
+      console.error(error);
+      router.navigateByUrl('/ad');
+      return throwError(error);
+    })
+  );
+};
 
 export default [
   {
@@ -17,6 +32,13 @@ export default [
     component: ComparatorComponent,
     resolve: {
       ads: () => inject(AdService).getAdsForComparator(),
+    },
+  },
+  {
+    path: 'detail/:adid',
+    component: DetailComponent,
+    resolve: {
+      ad: adResolver,
     },
   },
 ] as Routes;
