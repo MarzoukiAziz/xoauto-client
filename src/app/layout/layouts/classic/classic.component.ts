ff490a059bc948afaf5745aad9a8bf6439a7a738
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-10-10 19:52:14 +0100

    feat: track loading flag on template

--- src/app/layout/layouts/classic/classic.component.ts
+++ src/app/layout/layouts/classic/classic.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../common/header/header.component';
+import { FooterComponent } from '../../common/footer/footer.component';
+import { LoaderComponent } from 'src/app/shared/components/loader/loader.component';
+import { LoadingService } from 'src/app/shared/services/loading.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-classic',
   templateUrl: './classic.component.html',
-  styleUrl: './classic.component.css'
+  styleUrl: './classic.component.css',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    LoaderComponent,
+    RouterOutlet,
+    CommonModule,
+  ],
 })
 export class ClassicComponent {
-
+  constructor(public loader: LoadingService) {}
 }
