27ab271b7cc96c3206384bae9a58e77ece041276
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-10-10 19:49:16 +0100

    feat: change all components to be standalone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './layout/common/header/header.component';
-import { FooterComponent } from './layout/common/footer/footer.component';
-import { ClassicComponent } from './layout/layouts/classic/classic.component';
-import { LayoutComponent } from './layout/layout.component';
-import { TestComponent } from './modules/test/test.component';
 import {
   HTTP_INTERCEPTORS,
   provideHttpClient,
@@ -16,14 +11,7 @@ import {
 import { AuthInterceptor } from './modules/auth/auth.intercepter';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    ClassicComponent,
-    LayoutComponent,
-    TestComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [
     {
