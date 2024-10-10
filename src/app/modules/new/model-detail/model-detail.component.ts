f1792fff0559d6bc0b3b90ea6afbb5e60a99abca
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-10-10 22:00:19 +0100

    feat: create model details with top card

---
+++ src/app/modules/new/model-detail/model-detail.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Brand, Model, Version } from '../new.types';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-model-detail',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './model-detail.component.html',
+  styleUrl: './model-detail.component.css',
+})
+export class ModelDetailComponent {
+  brand: Brand;
+  model: Model;
+  versions: Version[];
+  selected_version: Version;
+  currency = environment.CURRENCY;
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.data.subscribe((data) => {
+      this.brand = data['brand'];
+      this.model = data['model'].ads[0];
+      this.versions = this.model.versions;
+      this.selected_version = this.versions[0];
+    });
+  }
+
+  changeVersion(newVersion) {
+    this.selected_version = newVersion;
+  }
+}
