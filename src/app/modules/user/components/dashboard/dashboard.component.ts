cc48ef967c51dfafdad1e6461725cb19de60edb0
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-10-13 01:18:39 +0100

    feat: show highlight in user dashboard

--- src/app/modules/user/components/dashboard/dashboard.component.ts
+++ src/app/modules/user/components/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PanelTitleComponent } from 'src/app/shared/components/panel-title/panel-title.component';
+import { HighLight } from '../../user.types';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,4 +9,14 @@ import { PanelTitleComponent } from 'src/app/shared/components/panel-title/panel
   standalone: true,
   imports: [PanelTitleComponent],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  highlight: HighLight;
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.data.subscribe((data) => {
+      this.highlight = data['highlight'];
+      console.log(this.highlight);
+    });
+  }
+}
